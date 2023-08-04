1f4ca8b05b30f7016fe6f7dd0124c09b6410a552
Alex Howden <ahowden27@gmail.com>
2023-08-04 12:42:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from creds import client_id, client_secret, redirect_uri
 #set ver to w for automatic relaunches, g for keybind relaunches
 ver = "w"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-read-currently-playing"))
-setup()
+setup(ver)
 
 while True:
     try:
         if sp.current_user_playing_track()['currently_playing_type'] == 'ad':
-            restart()
+            restart(ver)
         time.sleep(3)
     except:
         pass
