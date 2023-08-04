846494a55868147043ecf7dbf4ce15ec707710c3
Alex Howden <ahowden27@gmail.com>
2023-08-04 12:40:40 -0400

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import pyautogui
 from pynput import keyboard
+from spotifyadkiller import ver
 
 def setup():
     subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
@@ -11,6 +12,9 @@ def setup():
     pyautogui.hotkey('command', 'h')
     time.sleep(.25)
     pyautogui.hotkey('command', 'h')
+    if ver == 'g':
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()
 
 def on_press(key):
     if key == keyboard.Key.caps_lock:
@@ -19,16 +23,16 @@ def on_press(key):
         pyautogui.press('space')
         pyautogui.hotkey('command', 'h')
           
-def wRestart():
-    subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
-    time.sleep(2)
-    subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
-    time.sleep(2)
-    pyautogui.press('space')
-    time.sleep(.5)
-    pyautogui.hotkey('command', 'h')
-
-def gRestart():
-    subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
-    time.sleep(2)
-    subprocess.Popen(['open', '-a', "/Applications/Spotify.app", '-gj'])
+def restart():
+    if ver == 'w':
+        subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
+        time.sleep(2)
+        subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
+        time.sleep(2)
+        pyautogui.press('space')
+        time.sleep(.5)
+        pyautogui.hotkey('command', 'h')
+    else:
+        subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
+        time.sleep(2)
+        subprocess.Popen(['open', '-a', "/Applications/Spotify.app", '-gj'])
