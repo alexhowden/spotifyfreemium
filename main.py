9ac227ecd985b9eb4e0ee90aa4e47f0268f6d638
Alex Howden <ahowden27@gmail.com>
2023-08-04 12:26:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ def startup():
                                                  scope="user-read-currently-playing"))
 
 def on_press(key):
-        if key == keyboard.Key.caps_lock:
-            subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
-            time.sleep(.5)
-            pyautogui.press('space')
-            pyautogui.hotkey('command', 'h')
+    if key == keyboard.Key.caps_lock:
+        subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
+        time.sleep(.5)
+        pyautogui.press('space')
+        pyautogui.hotkey('command', 'h')
           
 def wRestart():
-  subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
-                time.sleep(2)
-                subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
-                time.sleep(2)
-                pyautogui.press('space')
-                time.sleep(.5)
-                pyautogui.hotkey('command', 'h')
+    subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
+    time.sleep(2)
+    subprocess.Popen(['open', '-a', "/Applications/Spotify.app"])
+    time.sleep(2)
+    pyautogui.press('space')
+    time.sleep(.5)
+    pyautogui.hotkey('command', 'h')
 
 def gRestart():
-  subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
-  time.sleep(2)
-  subprocess.Popen(['open', '-a', "/Applications/Spotify.app", '-gj'])
+    subprocess.call(['osascript', '-e', 'tell application "Spotify" to quit'])
+    time.sleep(2)
+    subprocess.Popen(['open', '-a', "/Applications/Spotify.app", '-gj'])
 
 
 startup()
